62d1defb3167fd67e87d956c170a966c1772cb2a
Mario Troiano <mariotroiano2@gmail.com>
2020-06-26 17:27:44 -0300

    modal carrito, view carrito y rutas terminadas

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/local', function(req, res, next) {
+  res.render('locals');
+});
+ 
+
+router.get('/cart', function(req, res, next) {
+   res.render('cart-detail');
+  
+});
+
 module.exports = router;
