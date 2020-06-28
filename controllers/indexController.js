b3a91baa2d3c6e71fd1a971d4f23c5ccd8e43be3
Mario Troiano <mariotroiano2@gmail.com>
2020-06-28 19:07:56 -0300

    llenando home con productos

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,21 @@
+const fs = require('fs')
+let products = require('../data/products.json')
+function readJson(file){
+  return  fs.readFileSync(file, {encoding : 'utf-8'})
+}
+
 let indexFunctions = {
 
 store : (req, res) => {
-    res.render('index');
+    // let productsCategorys = products.filter((valor, indiceActual, arreglo) => arreglo.indexOf(valor) === indiceActual);
+     let productsCategorys = products.filter(product => product.category)
+    // console.log(productsCategorys)
+    res.render('index', {productsCategorys : productsCategorys})
+    
 },
     
+
+
 cart : (req, res) => {
     res.render('cart-detail');
 },
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const indexController = require('../controllers/indexController')
+const indexController = require('../controllers/indexController');
+const { categorys } = require('../controllers/indexController');
 /* GET home page. */
-router.get('/', indexController.store)
+router.get('/', indexController.store )
+
+router.get('/cart', indexController.cart)
+
+
+
+
 
 ////////// esta ruta no sirve para nada es para probar el mapa de google maps/////////
 router.get('/local', indexController.locals)
 ////////// esta ruta no sirve para nada es para probar el mapa de google maps/////////
 
-router.get('/cart', indexController.cart)
 
 
 module.exports = router;
