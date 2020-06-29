c3c9e0d0917a940b911b0b6274f753525824627e
Mario Troiano <mariotroiano2@gmail.com>
2020-06-29 15:59:50 -0300

    metodo para eliminar duplicados en categorias, scroll en dropdown, colocar categorias reales en dropdown

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,30 +1,39 @@
 const fs = require('fs')
 let products = require('../data/products.json')
 function readJson(file){
-  return  fs.readFileSync(file, {encoding : 'utf-8'})
+    return  fs.readFileSync(file, {encoding : 'utf-8'})
 }
+// esta funcion borra las categorias dupicadas //
+function propertysNoRepeat (array){
+    arrayNoRepeat = []
+    array.forEach(element =>{
+        if(!arrayNoRepeat.includes(element.category)){
+            arrayNoRepeat.push(element.category)
+        }
+    });
+    return arrayNoRepeat
+}
+// esta funcion borra las categorias dupicadas //
 
 let indexFunctions = {
-
-store : (req, res) => {
-    // let productsCategorys = products.filter((valor, indiceActual, arreglo) => arreglo.indexOf(valor) === indiceActual);
-     let productsCategorys = products.filter(product => product.category)
-    // console.log(productsCategorys)
-    res.render('index', {productsCategorys : productsCategorys})
     
-},
+    store : (req, res) => {
+        let categorys = products.filter(product => product.category)
+        let productsCategorys = propertysNoRepeat(categorys)
+        res.render('index', {productsCategorys : productsCategorys})  
+    },
     
-
-
-cart : (req, res) => {
-    res.render('cart-detail');
-},
-   
- locals : (req, res) => {
-    res.render('locals');
-},
-        
-     
+    
+    
+    cart : (req, res) => {
+        res.render('cart-detail');
+    },
+    
+    locals : (req, res) => {
+        res.render('locals');
+    },
+    
+    
 }
 
 
