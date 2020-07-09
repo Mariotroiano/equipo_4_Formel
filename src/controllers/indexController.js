66c8f6be9beed75f7e1ea1c79e42e8fda29b4190
Mario Troiano <mariotroiano2@gmail.com>
2020-07-09 10:16:45 -0300

    crud finalizado + subida de fotos/productos con multer

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -3,10 +3,10 @@ let path = require('path');
 
 // const productsFilePath = path.join(__dirname, '../data/products.json');
 // const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-let productPruebaFilePath = path.join(__dirname, '../data/pruebasRegistro.json');
-let products2 = JSON.parse(fs.readFileSync(productPruebaFilePath, 'utf-8'));
+let productsFilePath = path.join(__dirname, '../data/products.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-let products = require('../data/products.json');
+// let products = require('../data/products.json');
 
 function readJson(file){
     return  fs.readFileSync(file, {encoding : 'utf-8'})
@@ -29,7 +29,7 @@ let indexFunctions = {
     store : (req, res) => {
         let categorys = products.filter(product => product.category)
         let productsCategorys = propertysNoRepeat(categorys)
-       
+        
         let offerProducts = products.filter(product => product.price <= 1200)
         res.render('index', { user : req.session.user, loginError : req.session.loginError, offerProducts})  
     },
@@ -43,53 +43,68 @@ let indexFunctions = {
     locals : (req, res) => {
         res.render('locals');
     },
-
+    
     products : (req, res)=>{
         
         res.render('products', {products : products})
     },
-  
+    
     productsCategory : (req, res)=>{
         res.render('products-category')
     },
     
     productsDetail : (req, res)=>{
         let productId = products.filter(product => product.id == req.params.productId)
-		res.render('products-detail', {productId : productId})
+        res.render('products-detail', {productId : productId})
     },
     
     createGet : (req, res)=>{
         res.render('products-create')
     },
-
+    
     create : (req, res)=>{
+        let position = products.length + 2
         let product = {
-           ...req.body
+            ...req.body,
+            id : position,
+            image : req.files[0].filename
+            
         }
-
+        
         console.log(product)
-        products2.push(product)
-
-        fs.writeFileSync(productPruebaFilePath, JSON.stringify(products2))
+        products.push(product)
+        
+        fs.writeFileSync(productsFilePath, JSON.stringify(products))
         res.redirect('/products/create') 
     },
-
+    
     edit : (req, res)=> {
         let productToEdit = products.filter(product => product.id == req.params.productId)
         res.render('products-edit', {productToEdit : productToEdit})
     },
-
+    
     update : (req, res)=> {
        
-
+        let productEdit = products.map(function(product){
+            if(product.id == req.params.productId){
+                
+                return {
+                    ...product, ...req.body,
+                    
+                }
+            }
+            return product
+        });
+        fs.writeFileSync(productsFilePath, JSON.stringify(productEdit));
+        res.redirect('/products')
     },
-
-    deleteProduct : (req, res) => {
-		let productDelete = products.filter(product => product.id != req.params.productId)
-		
-		fs.writeFileSync('products.json', JSON.stringify(productDelete), 'utf8');
-		res.redirect('/products')
-	}
+    
+    delete : (req, res) => {
+        let productDelete = products.filter(product => product.id != req.params.productId)
+        
+        fs.writeFileSync(productsFilePath, JSON.stringify(productDelete), 'utf8');
+        res.redirect('/products')
+    }
 }
 
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const indexController = require('../controllers/indexController');
-const { categorys } = require('../controllers/indexController');
+
 let authMiddlewares = require('../middlewares/authMiddleware');
-const middlewaresFunctions = require('../middlewares/authMiddleware');
+let uploadMiddleware = require('../middlewares/uploadMiddleware')
+
+var upload = uploadMiddleware
 
 /* GET home page. */
 router.get('/', indexController.store )
@@ -15,31 +17,17 @@ router.get('/products/category', indexController.productsCategory)
 router.get('/products/detail/:productId', indexController.productsDetail)
 
 router.get('/products/create', indexController.createGet)
-router.post('/products/create', indexController.create)
+router.post('/products/create',upload.any(), indexController.create)
 
 router.get('/products/edit/:productId', indexController.edit)
+
 router.put('/products/edit/:productId', indexController.update)
 
- router.delete('/products/delete/:productId', indexController.deleteProduct); 
+ router.delete('/products/delete/:productId', indexController.delete); 
 
+ router.get('/cart',authMiddlewares.loginMiddleware, indexController.cart)
 
-
-router.get('/cart',middlewaresFunctions.loginMiddleware, indexController.cart)
-
-
-
-
-
-
-
-
-
-
-
-////////// esta ruta no sirve para nada es para probar el mapa de google maps/////////
-router.get('/local', indexController.locals)
-////////// esta ruta no sirve para nada es para probar el mapa de google maps/////////
-
-
-
-module.exports = router;
+ ////////// esta ruta no sirve para nada es para probar el mapa de google maps/////////
+ router.get('/local', indexController.locals)
+ ////////// esta ruta no sirve para nada es para probar el mapa de google maps/////////
+ module.exports = router;
