349bc7bc0fda0057de23399ff964508ad1408a1f
Mario Troiano <mariotroiano2@gmail.com>
2020-08-27 18:01:52 -0300

    crud de admin terminado

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -15,6 +15,52 @@ let adminFunctions = {
         .then(([products, users, categorys])=>{
            res.render('admin/dashboard', {products : products, users : users, categorys : categorys})
         })
+    },
+    create : (req, res, next) =>{
+        res.render('admin/create')
+    },
+
+    add : (req, res, next)=>{
+      db.Product_category.create({
+          ...req.body
+      })
+      .then(category =>{
+          res.redirect('/')
+      })
+    },
+
+    edit : (req, res, next)=>{
+        db.Product_category.findByPk(req.params.categoryId)
+        .then(cat =>{
+            res.render('admin/edit', {category : cat})
+
+        })
+    },
+
+    update : (req, res, next) =>{
+        db.Product_category.update({
+            ...req.body
+        }, {
+            where : {
+                id : req.params.categoryId
+            }
+        })
+        .then(category => {
+            res.redirect('/')
+        })
+    },
+
+    delete : (req, res, next)=>{
+        db.Product_category.destroy({
+            where : {
+                id : {
+                    [Op.eq] : req.params.categoryId
+                } 
+            }
+        })
+        .then(category =>{
+            res.redirect('/')
+        })
     }
 }
 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,4 +4,15 @@ let adminController = require('../controllers/adminController')
 
 router.get('/', adminController.info)
 
+router.get('/create', adminController.create)
+router.post('/add', adminController.add)
+
+router.get('/edit/:categoryId', adminController.edit)
+router.put('/:categoryId', adminController.update)
+
+router.delete('/:categoryId', adminController.delete)
+
+
+
+
 module.exports = router
