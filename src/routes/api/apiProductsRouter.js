42796a0ca6172141c1954db3ef5bdb67db9d90e4
agustingauto <agustingauto95@gmail.com>
2020-09-04 03:45:05 -0300

    Expongo API y configuro ruta para consumir desde React

--- src/routes/api/apiProductsRouter.js
+++ src/routes/api/apiProductsRouter.js
@@ -4,5 +4,6 @@ let productsController = require('../../controllers/api/productsController')
 
 router.get('/', productsController.show)
 router.get('/categorys', productsController.categorys)
+router.get('/lastProduct', productsController.lastProduct)
 
 module.exports = router
