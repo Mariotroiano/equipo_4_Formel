c9651a55940ccfc6011ea43503166ccb8f37d006
Mario Troiano <mariotroiano2@gmail.com>
2020-09-16 19:23:28 -0300

    mercado pago de prueba y tabla pivot de carrito cargada

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,6 +1,12 @@
 let db = require('../db/models');
 const Op = db.Sequelize.Op
 let Cart = require('../custom-functions/cart')
+let mp = require('mercadopago')
+
+mp.configure({
+    sandbox: true,
+    access_token: process.env.MP_ACCESS_TOKEN
+});
 
 let cartFunctions = {
     addProduct : (req, res, next)=>{       
@@ -18,7 +24,7 @@ let cartFunctions = {
         })
         
     },
-
+    
     detailCart : (req, res, next) => {        
         if (!req.session.cart) {
             res.render('cart', {
@@ -32,7 +38,7 @@ let cartFunctions = {
         });
         
     },
-     
+    
     remove : (req, res, next)=>{        
         var cart = new Cart(req.session.cart ? req.session.cart : {});        
         cart.remove(req.params.productId);
@@ -42,69 +48,105 @@ let cartFunctions = {
     form : (req, res, next)=>{
         res.render('addressesForm')
     },
-
-    userAdresses : (req, res, next)=>{
-        let user = req.session.user
     
+    userAdresses :  (req, res, next)=>{
+        let user = req.session.user
+        let cart = new Cart (req.session.cart)
+        let products = cart.getItems()
+        let preference = {
+            back_urls : {
+                success : 'http://localhost:3030/products'
+            },
+            items: [
+                {
+                    title: 'Total',
+                    unit_price: cart.totalPrice,
+                    quantity: 1,
+                }
+            ]
+        };
+        
         db.Address.create({
             ...req.body,
             user_id : user.id
         })
-        .then(address =>{
-            req.session.cart = ""
-            res.render('purchase')
-        })
-        .catch(err =>{
-            console.log(err)
-            res.send('no se pudo guardar la direccion')
-        })
+        .then(userAdress=>{           
+                       
+            mp.preferences.create(preference)
+            .then(function(response){
+                // Crea un objeto de preferencia
+                
+                // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+                global.id = response.body.id;
+                res.redirect(response.body.sandbox_init_point)
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+            
+        })                      
+        
     },
-
-    prueba: (req, res, next)=>{
+    
+    prueba: async (req, res, next)=>{
         let user = req.session.user
-        let cart = req.session.cart
-
-        db.User.findByPk( user.id,{
+        let cart = new Cart( req.session.cart)
+        let products = cart.getItems()
+        
+        let userAddress = await db.User.findByPk( user.id,{
             include : [{association : 'Address'}]
-        })                     
-        .then(user=>{
-         
-            db.Cart.create({
-                total : cart.totalPrice,
-                shipping_address_id : user.Address[0].id,
-                user_id : user.id
-            })
-            .then(cart =>{      
-                   
-                res.render('/')
-            })
-            .catch(err=>{
-                console.log(err)
-                res.send('no se pudo guardar el carrito')
-            })
-        })    
-        .catch(err=>{
-            console.log(err)
-            res.send('no se encontro el usuario con su direccion')
+        })                 
+        
+        let cartCreated =  await db.Cart.create({
+            total : cart.totalPrice,
+            shipping_address_id : userAddress.Address[0].id,
+            user_id : user.id
         })
+        
+        let ultimo = await  db.Cart.findOne({
+            where : {
+                user_id : {
+                    [Op.eq] : user.id
+                }
+            },
+            order: [
+                ['created_at', 'DESC']
+            ] 
+        })        
+        
+        products.forEach(prod =>{
+            db.Cart_product.create( {                
+                cart_id : ultimo.id,
+                product_id : prod.item.id,
+                quantity : prod.quantity,
+                price : prod.price                
+            })
+        })
+        
+        try{                      
+            res.json(products)
+        }
+        catch(err){
+            res.send('no se pudo guardar el carrito')
+            
+        }
+        
     },
-
+    
     prueba2 : (req, res, next)=>{
+        let user = req.session.user
         db.Cart.findAll({
+            where : {
+                user_id : user.id
+            },
             include : [{association : 'cartProduct'},{association : 'User'}, {association : 'Adress'} ]
         })
         .then(carts=>{
             res.json(carts)
         })
-
-          db.Cart.findAll({
-            include : [{association : 'cartProduct'},{association : 'User'}, {association : 'Adress'} ]
-        })
-        .then(carts=>{
-            res.json(carts)
-        })
+        
     },
-
+    
     
 }
 
